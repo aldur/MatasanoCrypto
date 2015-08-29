f4b42e8e30d6808e918b97ce5bff14d077478346
Aldur <adrianodl@hotmail.it>
2015-08-29 15:42:14 +0200

    Add solution to #28

---
+++ matasano/mac.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env/ python
+# encoding: utf-8
+
+__author__ = 'aldur'
+
+"""MAC-related tools."""
+
+import matasano.hash
+
+
+def sha1_secret_prefix(key: bytes, message: bytes) -> bytes:
+    """
+    Authenticate the message by prefixing it with the secret key,
+    and returning the resulting SHA1 digest.
+
+    :param key: The secret key.
+    :param message: The message to be authenticated.
+    :return: The message authentication code.
+    """
+    return matasano.hash.sha1(
+        key + message
+    )
---
+++ matasano/test_mac.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env/ python
+# encoding: utf-8
+
+__author__ = 'aldur'
+
+import unittest
+
+import matasano.mac
+
+
+class MacTestCase(unittest.TestCase):
+    def test_sha1_secret_prefix(self):
+        # Foolish tests, for now.
+        f = matasano.mac.sha1_secret_prefix
+
+        m_one = f(b"KEY", b"YELLOW_SUBMARINE")
+        m_two = f(b"FOO", b"YELLOW_SUBMARINE")
+
+        self.assertNotEqual(m_one, m_two)
+
+
+if __name__ == '__main__':
+    unittest.main()
