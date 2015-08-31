bd4e4f8bf90a053ba3f22c04d9b0d0b4f5ae9b28
Aldur <adrianodl@hotmail.it>
2015-08-31 12:31:23 +0200

    Add HMAC

--- matasano/mac.py
+++ matasano/mac.py
@@ -6,33 +6,75 @@ __author__ = 'aldur'
 """MAC-related tools."""
 
 import functools
+
 import matasano.hash
+import matasano.util
 
 
 def _mac_secret_prefix(
         key: bytes,
         message: bytes,
-        mac
+        hash_function
 ) -> bytes:
     """
     Authenticate the message by prefixing it with the secret key,
     and returning the resulting MAC digest.
 
-    :param mac: The MAC function.
     :param key: The secret key.
     :param message: The message to be authenticated.
+    :param hash_function: The hash function.
     :return: The message authentication code.
     """
-    return mac(
+    return hash_function(
         key + message
     )
 
 sha1_secret_prefix = functools.partial(
     _mac_secret_prefix,
-    mac=matasano.hash.SHA1
+    hash_function=matasano.hash.SHA1
 )
 
 md4_secret_prefix = functools.partial(
     _mac_secret_prefix,
-    mac=matasano.hash.MD4
+    hash_function=matasano.hash.MD4
+)
+
+
+def _hmac(
+        key: bytes,
+        message: bytes,
+        hash_function,
+        block_size: int
+):
+    """
+    Return the HMAC signature of the message,
+    produced by using the given key and mac function.
+
+    :param key: The secret key.
+    :param message: The message to be authenticated.
+    :param hash_function: The MAC function.
+    :param block_size: The bytes length produced by the hash function.
+    :return: The message authentication code.
+    """
+    assert all((key, message, hash_function, block_size))
+
+    opad = bytes(0x5c for _ in range(block_size))
+    ipad = bytes(0x36 for _ in range(block_size))
+
+    if len(key) > block_size:  # Trimming
+        key = hash_function(key)
+    if len(key) < block_size:  # 0-Padding
+        key = key + bytes(block_size - len(key))
+
+    return hash_function(
+        matasano.util.xor(key, opad) + hash_function(
+            matasano.util.xor(key, ipad) + message
+        )
+    )
+
+
+hmac_sha1 = functools.partial(
+    _hmac,
+    hash_function=matasano.hash.SHA1,
+    block_size=64
 )
--- matasano/t/test_mac.py
+++ matasano/t/test_mac.py
@@ -4,6 +4,8 @@
 __author__ = 'aldur'
 
 import unittest
+import hmac
+import hashlib
 
 import matasano.mac
 
@@ -26,6 +28,21 @@ class MacTestCase(unittest.TestCase):
         m_two = f(b"FOO", b"YELLOW_SUBMARINE")
 
         self.assertNotEqual(m_one, m_two)
+
+    def test_sha1_hmac(self):
+        for k in [b"SECRET_KEY", bytes(200)]:
+            m = b"Yep, this is a message."
+
+            f = matasano.mac.hmac_sha1
+            signature = f(k, m)
+
+            truth = hmac.HMAC(
+                k, m, hashlib.sha1
+            ).digest()
+
+            self.assertEqual(
+                signature, truth
+            )
 
 
 if __name__ == '__main__':
